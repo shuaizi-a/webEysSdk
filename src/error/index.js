fe377794b8ec2e51e229896b8875967a800b59da
张帅 <77367582@qq.com>
2024-08-12 21:19:28 +0800

    feat:处理错误捕获

---
+++ src/error/index.js
@@ -0,0 +1,55 @@
+export default function error() {
+  // 捕获资源加载失败的错误： js css  img
+  window.addEventListener(
+    'error',
+    function (e) {
+      const target = e.target;
+      if (target.src || target.href) {
+        const url = target.src || target.href;
+        const reportData = {
+          type: 'error',
+          subType: 'resource',
+          url,
+          html: target.outerHTML,
+          pageUrl: window.location.href,
+          paths: e.path
+        };
+        // todo 发送错误信息
+        console.log(reportData);
+      }
+    },
+    true
+  );
+  // 捕获js错误
+  window.onerror = function (msg, url, lineNo, columnNo, error) {
+    const reportData = {
+      type: 'error',
+      subType: 'js',
+      msg,
+      url,
+      lineNo,
+      columnNo,
+      stack: error.stack,
+      pageUrl: window.location.href,
+      startTime: performance.now()
+    };
+    // todo 发送错误信息
+    console.log(reportData);
+  };
+  // 捕获promise错误  async await
+  window.addEventListener(
+    'unhandledrejection',
+    function (e) {
+      const reportData = {
+        type: 'error',
+        subType: 'promise',
+        reason: e.reason?.stack,
+        pageUrl: window.location.href,
+        startTime: e.timeStamp
+      };
+      // todo 发送错误信息
+      console.log(reportData);
+    },
+    true
+  );
+}
---
+++ src/performance/xhr.js
@@ -0,0 +1,41 @@
+export const originalProto = XMLHttpRequest.prototype;
+export const originalSend = originalProto.send;
+export const originalOpen = originalProto.open;
+
+function overwriteOpenAndSend() {
+  originalProto.open = function newOpen(...args) {
+    this.url = args[1];
+    this.method = args[0];
+    originalOpen.apply(this, args);
+  };
+  originalProto.send = function newSend(...args) {
+    this.startTime = Date.now();
+    const onLoaded = () => {
+      this.endTime = Date.now();
+      this.duration = this.endTime - this.startTime;
+      const { url, method, startTime, endTime, duration, status } = this;
+      const reportData = {
+        status,
+        duration,
+        startTime,
+        endTime,
+        url,
+        method: method.toUpperCase(),
+        type: 'performance',
+        success: status >= 200 && status < 300,
+        subType: 'xhr'
+      };
+      // todo 发送数据
+      console.log(reportData);
+
+      // 移除监听
+      this.removeEventListener('loadend', onLoaded, true);
+    };
+    // 监听请求完成
+    this.addEventListener('loadend', onLoaded, true);
+    originalSend.apply(this, args);
+  };
+}
+export default function xhr() {
+  overwriteOpenAndSend();
+}
